b6625123f19c49a27db706568eadc7b0ffeefaaa
Thadli Guerra <tg.ibarra64@gmail.com>
2025-03-17 15:01:38 -0300

    proyecto listo

--- src/components/ProductView.tsx
+++ src/components/ProductView.tsx
@@ -4,8 +4,11 @@ import { useStore } from "../store";
 import { ProductType } from "../schemas";
 
 export default function ProductView() {
-	/** State inicial */
+	/** States */
 	const [productoFinal, setProductoFinal] = useState<ProductType>({} as ProductType);
+	const addToCart = useStore(state => state.addToCart);
+	const items = useStore(state => state.items);
+	
 
 	/** Navigate y parametros desde la Url */
 	const navigate = useNavigate();
@@ -29,28 +32,54 @@ export default function ProductView() {
 		};
 
 		/** Existe dentro de la bd el producto ? */
-		if(productId !== actualProduct.id) {
+		if (productId !== actualProduct.id) {
 			const productExist = products.some(item => item.id === productId);
-			if(!productExist) {
+			if (!productExist) {
 				navigate("/")
 				return;
-			}	
+			}
 			setProductoFinal(products[productId - 1])
 			return;
-		} 
+		}
 		setProductoFinal(actualProduct);
 	}, [navigate, product]);
 
-	/** Listo */
-	useEffect(() => {
-		if(productoFinal && productoFinal.id) {
-			console.log(productoFinal);
-		}
-	}, [productoFinal])
+
+	/** Funciones */
+	function handlerClick() {
+		const { id, image, title, price, ...resto } = productoFinal;
+		addToCart({
+			id,
+			image,
+			title,
+			price,
+			quantity: 1
+		})
+	}
+
 
 	return (
-		<div className="container mx-auto w-full">
-			
+		<div className="container flex justify-center gap-30 items-center w-full mx-auto p-5">
+			<div
+				className="w-[700px] h-[700px] cursor-pointer p-5 rounded-sm flex justify-center items-center"
+			>
+				<img
+					src={`${productoFinal.image}`}
+					alt="Imagen del producto"
+					className="w-[50%] h-[50%]"
+				/>
+			</div>
+			<div className="w-[500px] h-[600px] border-2 border-gray-400 rounded-sm p-5 flex flex-col space-y-15">
+				<div className="text-center w-full flex flex-col space-y-15">
+					<p className="text-3xl font-bold ">{productoFinal.title}</p>
+					<p className="text-xl">{productoFinal.description}</p>
+					<p className="font-bold text-4xl">Precio: <span className="text-cyan-600">${productoFinal.price}</span></p>
+				</div>
+				<button
+					className="px-2 py-1 bg-blue-300 border rounded-sm cursor-pointer "
+					onClick={handlerClick}
+				>Agregar</button>
+			</div>
 		</div>
 	)
 }
